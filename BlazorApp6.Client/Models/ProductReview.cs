c425b1207628a6b55a69fc397730e96b88436729
Mian Muneeb <Mian Muneeb@DESKTOP-KF0OIQF>
2024-09-08 22:45:35 +0500

    Add project files.

---
+++ BlazorApp6.Client/Models/ProductReview.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp6
+{
+    public class ProductReview
+    {
+        [Required(ErrorMessage = "Product Name is required.")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Rating is required.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int Rating { get; set; }
+
+        [MinLength(20, ErrorMessage = "Review Description must be at least 20 characters long.")]
+        public string ReviewDescription { get; set; }
+
+        [Required(ErrorMessage = "Purchase Date is required.")]
+        [DataType(DataType.Date)]
+        [FutureDateNotAllowed]
+        public DateTime PurchaseDate { get; set; }
+
+        [Required(ErrorMessage = "Recommendation is required.")]
+        public bool WouldRecommend { get; set; }
+
+        [Required(ErrorMessage = "You must agree to the terms.")]
+        public bool AgreeToTerms { get; set; }
+    }
+
+    public class FutureDateNotAllowedAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.Date <= DateTime.Now.Date;
+            }
+            return true;
+        }
+    }
+}
---
+++ BlazorApp6.Client/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorApp6.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
+
